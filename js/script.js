fe744d0d9d22d60cf71caebe36d859e9746271b8
sarah_b <sarah.am.barton@gmail.com>
2023-03-07 14:49:01 -0800

    Added functions to confirm if word in progress matches correct word

--- js/script.js
+++ js/script.js
@@ -53,5 +53,41 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        guessedLettersDisplay();
+        updateWordInProgress(guessedLetters);
+
     }
+};
+
+const guessedLettersDisplay = function () {
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    // console.log(wordArray);
+    const revealWord = [];
+   for (const letter of wordArray) {
+     if (guessedLetters.includes(letter)) {
+        revealWord.push(letter.toUpperCase());
+    } else {
+        revealWord.push("●");
+    }
+   }
+    wordInProgress.innerText = revealWord.join("");
+    confirmWin();
+
+};
+
+const confirmWin = function () {
+    if (wordInProgress.innerText === word.toUpperCase()) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    } 
 };
